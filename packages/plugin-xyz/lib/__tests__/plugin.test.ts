db50972508f533771d797f59e302ca51bead80dc
Andrey Sokolov <falconandy@yandex.ru>
2021-07-19 11:50:21 +0400

    chore: fixed XYZ logic (3)

--- packages/plugin-xyz/lib/__tests__/plugin.test.ts
+++ packages/plugin-xyz/lib/__tests__/plugin.test.ts
@@ -3,6 +3,6 @@ import { PluginXYZ } from '..';
 
 describe('PluginXYZ', () => {
   test('should succeed', () => {
-    expect(PluginXYZ()).toEqual('PluginXYZ_SdkA_2');
+    expect(PluginXYZ()).toEqual('PluginXYZ_SdkA_3');
   });
 });
